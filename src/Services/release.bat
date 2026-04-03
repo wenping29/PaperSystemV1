d099e3f5436712867c9498071647f4433bcc3a48
wenping29 <wenping29@163.com>
2026-04-03 16:27:53 +0800

    新增批量打包，批量执行Linux启动脚本

--- src/Services/release.bat
+++ src/Services/release.bat
@@ -17,6 +17,8 @@ cd ../UserService
 dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true  -o ..\..\Publish\UserService
 cd ../WritingService
 dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true  -o ..\..\Publish\WritingService
+cd ../FileService
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true  -o ..\..\Publish\FileService
 pause
 
 
