23d90b5cc3c3b7c9f12ec5c600fc537282641a1c
wenping29 <wenping29@163.com>
2026-04-08 14:16:05 +0800

    上传每章剧情

--- src/Services/PaperSystemApi/UserServices/Helpers/JwtTokenGenerator.cs
+++ src/Services/PaperSystemApi/UserServices/Helpers/JwtTokenGenerator.cs
@@ -1,6 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace PaperSystemApi.UserServices.Helpers
